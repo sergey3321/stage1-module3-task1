9eb74053bd18b13455d55524c6922b1395c0de16
sergey3321 <mail.sergey21@gmail.com>
2023-09-11 17:43:19 +0300

    stage1-module3-task1

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -6,16 +6,21 @@ import java.util.List;
 public class Generics {
 
     //TODO: Refactor Method-1
-    public List boxingMethod(String name) {
-        List firstList = new ArrayList<>();
+    public static List<List<String>> boxingMethod(String name) {
+        List<String> firstList = new ArrayList<>();
         firstList.add(name);
-        List secondList = new ArrayList<>();
+        List<List<String>> secondList = new ArrayList<>();
         secondList.add(firstList);
         return secondList;
     }
 
+//    //TODO: Refactor Method-2
+//    public Object genericMethod(Object data) {
+//        return data;
+//    }
+
     //TODO: Refactor Method-2
-    public Object genericMethod(Object data) {
+    public static <T> T genericMethod(T data) {
         return data;
     }
 
